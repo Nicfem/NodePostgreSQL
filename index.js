8029642983593585347939454955e2f5c4baa49f
Nicfem <sosiska2004dota2@gmail.com>
2022-08-23 23:03:10 +0300

    eslinter and models

--- index.js
+++ index.js
@@ -2,27 +2,31 @@ require('dotenv').config()
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const models = require('./models/models')
 const path = require('path')
+const sequelize = require('./db')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
 app.use(cors({   
-	credentials: true, 
-	origin: 'http://localhost:3000' 
+    credentials: true, 
+    origin: 'http://localhost:3000' 
 }))
 app.use(express.json({ extended: true}))
 app.use(cookieParser())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use('/api', require('./routes/index'))
 
-const start = async () => { 
-	try {
-		app.listen(PORT, () => console.log('Server started on port ' + PORT))
-	} catch (e) {
-		console.log(e.message)
-	}
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e.message)
+    }
 }
 
 start().catch(console.dir)
